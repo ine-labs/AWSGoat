a89a303119c0ea2dccc7bebd44862f2709ead12a
srijaallam <srija.allam10@gmail.com>
2023-01-04 16:43:32 -0500

    Update main.tf

--- modules/module-2/main.tf
+++ modules/module-2/main.tf
@@ -9,7 +9,7 @@ terraform {
 
 provider "aws" {
   region = "us-west-1"
-  shared_credentials_file = "/Users/ec2-user/.aws/credentials"
+  shared_credentials_file = "/home/ec2-user/.aws/credentials"
   profile                 = "jenkins_user"
 }
 
