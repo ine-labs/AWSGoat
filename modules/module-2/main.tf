7db79bb0566a644f0343c00424e2300522bf2553
Rishappreet Singh Moonga <63499257+Rishappreet-Singh@users.noreply.github.com>
2025-01-31 17:08:31 +0530

    Update Launch Configuration to Launch Template

--- modules/module-2/main.tf
+++ modules/module-2/main.tf
@@ -347,22 +347,32 @@ data "aws_ami" "ecs_optimized_ami" {
 }
 
 
+resource "aws_launch_template" "ecs_launch_template" {
+  name_prefix   = "ecs-launch-template-"
+  image_id      = data.aws_ami.ecs_optimized_ami.id
+  instance_type = "t2.micro"
 
-resource "aws_launch_configuration" "ecs_launch_config" {
-  image_id             = data.aws_ami.ecs_optimized_ami.id
-  iam_instance_profile = aws_iam_instance_profile.ecs-instance-profile.name
-  security_groups      = [aws_security_group.ecs_sg.id]
-  user_data            = data.template_file.user_data.rendered
-  instance_type        = "t2.micro"
+  iam_instance_profile {
+    name = aws_iam_instance_profile.ecs-instance-profile.name
+  }
+
+  vpc_security_group_ids = [aws_security_group.ecs_sg.id]
+  user_data              = base64encode(data.template_file.user_data.rendered)
 }
+
 resource "aws_autoscaling_group" "ecs_asg" {
-  name                 = "ECS-lab-asg"
-  vpc_zone_identifier  = [aws_subnet.lab-subnet-public-1.id]
-  launch_configuration = aws_launch_configuration.ecs_launch_config.name
-  desired_capacity     = 1
-  min_size             = 0
-  max_size             = 1
+  name                = "ECS-lab-asg"
+  vpc_zone_identifier = [aws_subnet.lab-subnet-public-1.id]
+  desired_capacity    = 1
+  min_size            = 0
+  max_size            = 1
+
+  launch_template {
+    id      = aws_launch_template.ecs_launch_template.id
+    version = "$Latest"
+  }
 }
+
 
 resource "aws_ecs_cluster" "cluster" {
   name = "ecs-lab-cluster"
